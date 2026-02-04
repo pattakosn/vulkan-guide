c9dbbfc7892c0effa9f774528a9bdba953f68c09
N. Pattakos <pattakosn@fastmail.com>
2026-02-04 23:39:46 +0100

    towards vkInstance and vkDevice

--- src/vk_engine.h
+++ src/vk_engine.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <vk_types.h>
+#include <vector>
 
 class VulkanEngine {
 public:
@@ -11,9 +12,22 @@ public:
 	bool _isInitialized{ false };
 	int _frameNumber {0};
 
-	VkExtent2D _windowExtent{ 1700 , 900 };
+	VkExtent2D _windowExtent{ 1280 , 960 };
 
 	struct SDL_Window* _window{ nullptr };
+
+	VkInstance _instance;// Vulkan library handle
+	VkDebugUtilsMessengerEXT _debug_messenger;// Vulkan debug output handle
+	VkPhysicalDevice _chosenGPU;// GPU chosen as the default device
+	VkDevice _device; // Vulkan device for commands
+	VkSurfaceKHR _surface;// Vulkan window surface
+
+	VkSwapchainKHR _swapchain;
+	VkFormat _swapchainImageFormat;
+
+	std::vector<VkImage> _swapchainImages;
+	std::vector<VkImageView> _swapchainImageViews;
+	VkExtent2D _swapchainExtent;
 
 	//initializes everything in the engine
 	void init();
@@ -26,4 +40,13 @@ public:
 
 	//run main loop
 	void run();
+
+private:
+	bool bUseValidationLayers{true};
+	void init_vulkan();
+	void init_swapchain();
+	void init_commands();
+	void init_sync_structures();
+	void create_swapchain(uint32_t width, uint32_t height);
+	void destroy_swapchain();
 };
